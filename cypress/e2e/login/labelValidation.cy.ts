6768747ff603441acc0889f8c160b6a902f140a8
Jarin Manuvel Mathew <jarinmanuvel@hotmail.com>
2024-10-19 19:38:43 -0400

    Updates Scripts to validate the labels in the sign up page

--- cypress/e2e/login/labelValidation.cy.ts
+++ cypress/e2e/login/labelValidation.cy.ts
@@ -1,27 +1,26 @@
 describe('Given the user is in the sign up page', () => {
     beforeEach(() => {
-      // Navigate to the login page before each test
-      cy.visit('https://app.qa.nesto.ca/signup')
-      
-      //accept the pop up
-      cy.get('#didomi-notice-agree-button').click() 
+        // Navigate to the login page before each test
+        cy.visit('https://app.qa.nesto.ca/signup')
 
-      cy.on ('uncaught:exception', (err, runnable) => {
-        return false
+        //accept the pop up
+        cy.get('#didomi-notice-agree-button').click()
+
+        cy.on('uncaught:exception', (err, runnable) => {
+            return false
         })
     })
-   /* afterEach(function () { 
+    afterEach(function () {
         if (this.currentTest?.state === 'failed') {
-          cy.screenshot(this.currentTest.title); // Take a screenshot if the test fails
+            cy.screenshot(this.currentTest.title); // Take a screenshot if the test fails
         }
-      })
-    */
+    })
+
 
     //Validates the labels
-    it('When User Views all the fields Then they should see all required labels correctly displayed', () =>{
-      cy.validateLabels()
-      cy.contains('Create a nesto account').should('be.visible')
+    it('When User Views all the fields Then they should see all required labels correctly displayed', () => {
+        cy.validateLabels()
+        cy.contains('Create a nesto account').should('be.visible')
 
     })
-  })
-  
+})
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -22,36 +22,43 @@ Cypress.Commands.add('fillSignupForm', () => {
     // Confirm the password
     cy.get('#passwordConfirm').type('Password@12345678')
 
+    //Selecting the Province to Ontario from the drop down
     cy.get('#select_province').click()
     cy.get('.react-select__menu.css-18td19-menu').should('be.visible').contains('Ontario').click();
-    
+
     //click on checkbox
     cy.get('#checkbox_leadDistributeConsentAgreement').check()
 })
 
+
 Cypress.Commands.add('validateLabels', () => {
 
-    // First Name Label Check in DOM
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div.sc-bdnxRM.sc-gtsrHT.hayblG.gfuSqG > div.sc-bdnxRM.dqWAcG > div > div.text-inputcomponent__Placeholder-n1w94z-2.bpgCcN').should('exist').and('be.visible');
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div.sc-bdnxRM.sc-gtsrHT.hayblG.gfuSqG > div.sc-bdnxRM.dqWAcG > div > div.text-inputcomponent__Placeholder-n1w94z-2.bpgCcN').should('contain.text', 'First Name')
+    // First Name Label Check 
+    cy.get('[data-test-id="input_label-firstName"]').should('exist');
+    cy.get('[data-test-id="input_label-firstName"]').should('contain.text', 'First name')
 
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div.sc-bdnxRM.sc-gtsrHT.hayblG.gfuSqG > div.sc-bdnxRM.ktZkOc > div > div.text-inputcomponent__Placeholder-n1w94z-2.bpgCcN').should('exist').and('be.visible')
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div.sc-bdnxRM.sc-gtsrHT.hayblG.gfuSqG > div.sc-bdnxRM.ktZkOc > div > div.text-inputcomponent__Placeholder-n1w94z-2.bpgCcN').should('contain.text', 'Last Name')
+    // Last Name Label Check
+    cy.get('[data-test-id="input_label-lastName"]').should('exist');
+    cy.get('[data-test-id="input_label-lastName"]').should('contain.text', 'Last name')
+    
+    // Email Label Check
+    cy.get('[data-test-id="input_label-email"]').should('exist');
+    cy.get('[data-test-id="input_label-email"]').should('contain.text', 'Email')
 
+    // Mobile Phone Number Label Check
+    cy.get('[data-test-id="input_label-phone"]').should('exist');
+    cy.get('[data-test-id="input_label-phone"]').should('contain.text', 'Mobile phone number')
 
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div:nth-child(3) > div > div > div.text-inputcomponent__Placeholder-n1w94z-2.bpgCcN').should('exist').and('be.visible')
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div:nth-child(3) > div > div > div.text-inputcomponent__Placeholder-n1w94z-2.bpgCcN').should('contain.text', 'Email')
+    // Password Label Check
+    cy.get('[data-test-id="input_label-password"]').should('exist');
+    cy.get('[data-test-id="input_label-password"]').should('contain.text', 'Password')
 
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div:nth-child(4) > div.phonecomponent__Wrapper-jl1zyj-0.csBWUM > div > div > div.phone-inputcomponent__Placeholder-sc-176r2w-2.dsvpDF').should('exist').and('be.visible')
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div:nth-child(4) > div.phonecomponent__Wrapper-jl1zyj-0.csBWUM > div > div > div.phone-inputcomponent__Placeholder-sc-176r2w-2.dsvpDF').should('contain.text', 'Mobile phone number')
+    // Confirm Password Label Check
+    cy.get('[data-test-id="input_label-passwordConfirm"]').should('exist');
+    cy.get('[data-test-id="input_label-passwordConfirm"]').should('contain.text', 'Confirm password')
 
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div:nth-child(4) > div:nth-child(2) > div > div.text-inputcomponent__Placeholder-n1w94z-2.bpgCcN').should('exist').and('be.visible')
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div:nth-child(4) > div:nth-child(2) > div > div.text-inputcomponent__Placeholder-n1w94z-2.bpgCcN').should('contain.text', 'Password')
-
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div:nth-child(4) > div:nth-child(4) > div > div.text-inputcomponent__Placeholder-n1w94z-2.bpgCcN').should('exist').and('be.visible')
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div:nth-child(4) > div:nth-child(4) > div > div.text-inputcomponent__Placeholder-n1w94z-2.bpgCcN').should('contain.text', 'Confirm password')
-
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div:nth-child(4) > div.sc-bdnxRM.selectcomponent__Wrapper-pyxzdc-0.jEPCdM.hACITx > div > div.selectcomponent__Placeholder-wgyntw-0.hokFBo').should('exist').and('be.visible')
-    cy.get('#app > div.sc-bdnxRM.sc-gtsrHT.content-with-headerstyles__Wrapper-sc-55h4ny-0.bbHHmq.gfuSqG.jVANSL > div.sc-bdnxRM.sc-gtsrHT.page-containercomponent__GreyBackgroundWrapper-sc-1s96hju-1.gEQkDQ.gfuSqG.ktVOIF > div > div > form > div > div.sc-bdnxRM.eZkqhv > div:nth-child(4) > div.sc-bdnxRM.selectcomponent__Wrapper-pyxzdc-0.jEPCdM.hACITx > div > div.selectcomponent__Placeholder-wgyntw-0.hokFBo').should('contain.text', 'Province')
+    // Province Label Check
+    cy.get('[data-test-id="select_label-province"]').should('exist');
+    cy.get('[data-test-id="select_label-province"]').should('contain.text', 'Province')
 
 })
