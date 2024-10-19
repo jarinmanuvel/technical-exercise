d1da47ceb5370f1148a72f23afbf281f5fc5e5d6
Jarin Manuvel Mathew <jarinmanuvel@hotmail.com>
2024-10-19 01:05:58 -0400

    updated the pageload test

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://app.qa.nesto.ca/login', 
+    specPattern: 'cypress/e2e/**/*.cy.ts',
+    supportFile: 'cypress/support/e2e.ts',
   },
 });
--- cypress/e2e/login/pageload.cy.ts
+++ cypress/e2e/login/pageload.cy.ts
@@ -1,5 +1,7 @@
-describe('psge load success', () => {
+describe('psge load validation', () => {
   it('passes', () => {
     cy.visit('https://app.qa.nesto.ca/login')
+    cy.get('#didomi-notice-agree-button').click() //clicking to accept the pop up window
+    cy.get('#loginPage_signUp').click() //navigates to sign up page
   })
 })
