29313efe5ee39d239a9cc38b7dc03827796664ad
Jarin Manuvel Mathew <jarinmanuvel@hotmail.com>
2024-10-20 12:33:10 -0400

    Updated pageLoad file with additional validations
    Additional validations for login page was added to verify the title, email and password fields and login button

--- cypress/e2e/login/pageload.cy.ts
+++ cypress/e2e/login/pageload.cy.ts
@@ -1,34 +1,64 @@
 describe('Given User Navigates to the Login URL', () => {
-  context('When User clicks on Signup Link', () => {
+  context('When User views the Login Page', () => {
     beforeEach(() => {
       cy.on('uncaught:exception', (err, runnable) => {
         return false
-      })
-    })
+      });
+      // Navigate to the login page before each test
+      cy.visit('https://app.qa.nesto.ca/login')
+      // Clicking to accept the pop-up window
+      cy.get('#didomi-notice-agree-button').click()
+    });
 
     afterEach(function () {
       if (this.currentTest?.state === 'failed') {
         // Take a screenshot if the test fails
         cy.screenshot(this.currentTest.title)
       }
-    })
+    });
 
-    it('Then User must be navigated to the Signup Page', () => {
-      //navigates to the login page
-      cy.visit('https://app.qa.nesto.ca/login')
+    it('Then the title, fields, and login button should be visible', () => {
+      // Validate the login page title
+      cy.contains('Log in to your account').should('be.visible')
 
-      //clicking to accept the pop up window
-      cy.get('#didomi-notice-agree-button').click()
+      // Validate the presence of the email and password fields
+      cy.get('#email').should('be.visible')
+      cy.get('#password').should('be.visible')
 
-      //navigates to sign up page
-      cy.get('#loginPage_signUp').click()
-
-      //assertion to validate the URL
-      cy.url().should('include', '/signup')
-
-      //assertion to validate the content
-      cy.contains('Create a nesto account').should('be.visible')
-
+      // Validate the presence of the login button
+      cy.get('#form_signup_login').should('be.visible')
     })
   })
-})
+
+  context('When User clicks on Signup Link', () => {
+    beforeEach(() => {
+      cy.on('uncaught:exception', (err, runnable) => {
+        return false
+      });
+    });
+
+    afterEach(function () {
+      if (this.currentTest?.state === 'failed') {
+        // Take a screenshot if the test fails
+        cy.screenshot(this.currentTest.title)
+      }
+    });
+
+    it('Then User must be navigated to the Signup Page', () => {
+      // Navigates to the login page
+      cy.visit('https://app.qa.nesto.ca/login')
+
+      // Clicking to accept the pop-up window
+      cy.get('#didomi-notice-agree-button').click()
+
+      // Navigates to the sign up page
+      cy.get('#loginPage_signUp').click()
+
+      // Assertion to validate the URL
+      cy.url().should('include', '/signup')
+
+      // Assertion to validate the content
+      cy.contains('Create a nesto account').should('be.visible')
+    });
+  });
+});
